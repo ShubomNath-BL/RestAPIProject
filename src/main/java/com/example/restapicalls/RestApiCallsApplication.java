9d4f9d8b01e85ca0ded393cec0939ed9891fff0a
ShubomNath-BL <shubhom.nath@bridgelabz.com>
2022-08-28 12:57:41 +0530

    Started: Project is just started

---
+++ src/main/java/com/example/restapicalls/RestApiCallsApplication.java
@@ -0,0 +1,14 @@
+package com.example.restapicalls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiCallsApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Bridgelabz");
+        SpringApplication.run(RestApiCallsApplication.class, args);
+    }
+
+}
